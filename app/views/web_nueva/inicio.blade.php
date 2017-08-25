4d58492683661b81537a06c1ffa0b8ba81bcc627
unknown <llrocha@9DJ-1626.sds_domain_1.local>
2017-08-25 09:46:34 -0300

    web nueva

--- app/views/web_nueva/inicio.blade.php
+++ app/views/web_nueva/inicio.blade.php
@@ -6,8 +6,8 @@
     <div class="container">
      <div class="owl-carousel col-lg-12">
       @if($partidosDiarios->count() > 0)
-         @include('web_nueva.partials.pxp')
          @include('web_nueva.partials.resultados')
+         @include('web_nueva.partials.pxp')
          @include('web_nueva.partials.proximos')
         
       @endif  
