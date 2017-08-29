6f5a0fdfda29d5f05eb0573dda29bf72466916d5
leandroleonelrocha <rochaleandroleonel@gmail.com>
2017-08-29 19:28:23 -0300

    web nueva

--- app/views/web_nueva/competencias/anteriores.blade.php
+++ app/views/web_nueva/competencias/anteriores.blade.php
@@ -5,67 +5,45 @@
  <div class="container">
 
         <div class="row">
-
-
-          <!-- Content -->
           <div class="content col-md-8">
-      {{ Form::open(array('route' => 'calendario', 'method' => 'GET'))}}      
-      <div class="card card--has-table">
-          <div class="card__header card__header--has-btn">
-            <h4>Seleccione Temporada</h4>
-            <!-- Result Filter -->
-            <ul class="team-result-filter">
-              <li class="team-result-filter__item">
-                <select class="form-control input-xs temporada_id">
-                  <option >TEMPORADA</option>
-                  @foreach($temporadas as $temporada)
-                  <option value="{{$temporada->id}}">{{$temporada->nombre_temporada}}</option>
-                  @endforeach 
-                </select>
-              </li>
-
-              <li class="team-result-filter__item">
-                <select class="form-control input-xs serie_id">
-                   <option >SERIE</option>
-                  @foreach($series as $serie)
-                  <option value="{{$serie->id}}"> {{$serie->nombre_serie}}</option>
-                  @endforeach
-                
-                </select>
-              </li>
-
-
-              <li class="team-result-filter__item">
-                <select class="form-control input-xs torneo_id" name="torneo_id">
-                 
-                </select>
-              </li>
-              
-              <li class="team-result-filter__item">
-               {{ Form::submit('Acceder', array('class' => 'btn btn-default btn-xs'))}}
-
-            </li>
-            </ul>
-            <!-- Result Filter / End -->
-            
+            <div class="card">
+              <div class="card__header">
+                <h4>Seleccione Temporada</h4>
+              </div>
+              <div class="card__content">
+                 {{ Form::open(array('route' => 'calendario', 'method' => 'GET'))}}    
+                  <div class="form-group form-group--sm">
+                    <select class="form-control temporada_id">
+                        <option >TEMPORADA</option>
+                        @foreach($temporadas as $temporada)
+                        <option value="{{$temporada->id}}">{{$temporada->nombre_temporada}}</option>
+                        @endforeach 
+                    </select>
+                  </div>
+                  <div class="form-group form-group--sm">
+                    <select class="form-control serie_id">
+                        <option >SERIE</option>
+                        @foreach($series as $serie)
+                        <option value="{{$serie->id}}"> {{$serie->nombre_serie}}</option>
+                        @endforeach
+                    </select>
+                  </div>
+                   <div class="form-group form-group--sm">
+                    <select name="torneo_id" class="form-control torneo_id">
+                        <option >TORNEO</option>
+                    </select>
+                  </div>
+                  <div class="">
+                    <button type="submit" class="btn btn-default btn-lg btn-block">Aceptar</button>
+                  </div>
+                 {{ Form::close() }}
+              </div>
+            </div>
           </div>
-          
-        </div>
-        {{ Form::close() }}
-        <br><br>
-            <!-- Posts List -->
-           
-            <!-- Posts List / End -->
-
-            <!-- Post Pagination -->
-            
-            <!-- Post Pagination / End -->
-            
-
-          </div>
+        
           @include('web_nueva.template.sidebar')
 
-</div>
+      </div>
 </div>
 </div>
 @endsection
