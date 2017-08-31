5239f809ec1b9f380d3b5cce47e2f06036c23f5d
hollowmanf <fernando@keepers.com.ar>
2017-08-31 10:35:20 -0300

    diseño de staf y jugadores de carrera

--- app/views/web_nueva/equipos/detalle_staff.blade.php
+++ app/views/web_nueva/equipos/detalle_staff.blade.php
@@ -55,16 +55,33 @@
                       <tr>
                         
                         <td class="team-roster-table__number">Carrera:</td> 
-                        <td class="team-roster-table__name"><h4>equipos donde jugó en temporadas anteriores. Podemos ya poner lo
-que figura en el sistema. Habría que hacer una tabla con columnas para poner los
-siguientes datos: temporada, serie, nombre del equipo donde jugó, nº de camiseta con
-el que jugó, nacionalidad (la sigla de 3 letras), notas (para poner “desde” o
-“hasta” si se incorporó al equipo luego de presentado el O2 o si lo dieron de baja
-antes de finalizar la temporada). Adjunto un par de capturas como ejemplo</h4></td>
+                        
                         
                       </tr>
                       
                     </tbody>
+                  </table>
+                  <table class="table table--lg team-roster-table">
+                    <thead>
+                      <tr>
+                        <th class="team-roster-table__name">Temporada</th>
+                        <th class="team-roster-table__name">Serie</th>
+                        <th class="team-roster-table__name">Equipo</th>
+                        <th class="team-roster-table__name">Función</th>
+                        <th class="team-roster-table__name">Notas</th>
+                      </tr>
+  
+</thead>
+                    <tbody>
+                      <tr>
+                        <td class="team-roster-table__name"><font color="#000000">xxxx</font></a></td>    
+                        <td class="team-roster-table__name"><font color="#000000">xxxx</font></a></td>
+                        <td class="team-roster-table__name"><font color="#000000">xxxx</font></a></td>
+                        <td class="team-roster-table__name"><font color="#000000">xxxx</font></a></td>
+                        <td class="team-roster-table__name"><font color="#000000">xxxx</font></a></td>
+                      </tr>
+                      
+                     </tbody>
                   </table>
                 </div>
 
