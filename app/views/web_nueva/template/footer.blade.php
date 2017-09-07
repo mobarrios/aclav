d77caa61355146dd3f09996e472480aa4f7e4b5b
hollowmanf <fernando@keepers.com.ar>
2017-09-07 11:48:57 -0300

    footer

--- app/views/web_nueva/template/footer.blade.php
+++ app/views/web_nueva/template/footer.blade.php
@@ -7,8 +7,8 @@
           <div class="sponsors">
             <ul class="sponsors-logos">
              @foreach(Sponsor::all() as $sponsor)
-              <li class="sponsors__item blanco">
-                <a href="{{$sponsor->url}}" target="_blank"> <img src="uploads/contenidos/sponsor/{{$sponsor->imagen}}" alt=""></a>
+              <li class="sponsors__item">
+                <a href="{{$sponsor->url}}" target="_blank"> <img src="uploads/contenidos/sponsor/{{$sponsor->imagen}}" class=" blanco"></a>
               </li>
              @endforeach
             </ul>
