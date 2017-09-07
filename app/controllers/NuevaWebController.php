049ba4b9155baf4845fe180647b02290876141cb
leandroleonelrocha <rochaleandroleonel@gmail.com>
2017-09-07 11:58:28 -0300

    modificacion logos responsive

--- app/controllers/NuevaWebController.php
+++ app/controllers/NuevaWebController.php
@@ -385,9 +385,22 @@ class NuevaWebController extends BaseController
 		$data['torneo'] = Torneos::find(Session::get('torneo_id'));
 		if($data['partido']->estado != '' || $data['partido']->estado != 0){
 			$data['jugadores_locales'] = BuenaFeBis::where('partido_id', $data['partido']->id)->where('equipo_id', $data['partido']->local_equipo_id->id)->get();
+			/*
+			$data['jugadores_locales'] = DB::table('buena_fe_bis')
+            ->join('buena_fe', 'buena_fe_bis.buena_fe_id', '=', 'buena_fe.id')
+            ->where('partido_id', $data['partido']->id)
+            ->where('equipo_id', $data['partido']->local_equipo_id->id)
+            ->orderBy('nro','ASC')
+            ->get();
+				*/
+           	
 			$data['staff_local'] = BuenaFeStaffBis::where('partido_id', $data['partido']->id)->where('equipo_id', $data['partido']->local_equipo_id->id)->get();
 			$data['jugadores_visitantes'] = BuenaFeBis::where('partido_id', $data['partido']->id)->where('equipo_id',$data['partido']->visita_equipo_id->id)->get();
 			$data['staff_visitante'] = BuenaFeStaffBis::where('partido_id', $data['partido']->id)->where('equipo_id', $data['partido']->visita_equipo_id->id)->get();	
+
+
+
+
 		}
 		$data['torneo_fase'] = TorneoFaseLegPartido::where('partido_id', $data['partido']->id)->get()->first();
 		$data['sesion_calendario'] = 1;
