2117d76c12d31bde47418593ac6e73ea6be83e24
unknown <llrocha@9DJ-1626.sds_domain_1.local>
2017-11-02 13:34:29 -0300

    update informacion partido

--- app/views/web_nueva/competencias/informacion.blade.php
+++ app/views/web_nueva/competencias/informacion.blade.php
@@ -89,9 +89,9 @@
           </div>
           <div class="card__content">
             <div class="glossary">
-              <div class="glossary__item"><span class="glossary__abbr">Torneo:</span> {{$torneo_fase->TorneoFaseLeg->fase->Torneo->nombre_torneo}} </div>
-              <div class="glossary__item"><span class="glossary__abbr">Fase:</span> {{$torneo_fase->TorneoFaseLeg->fase->nombre}} </div>
-              <div class="glossary__item"><span class="glossary__abbr">Etapa:</span> {{$torneo_fase->TorneoFaseLeg->nombre}} </div>
+              <div class="glossary__item"><span class="glossary__abbr">Torneo:</span> {{ isset($torneo_fase->TorneoFaseLeg->fase->Torneo->nombre_torneo) ? $torneo_fase->TorneoFaseLeg->fase->Torneo->nombre_torneo : '' }} </div>
+              <div class="glossary__item"><span class="glossary__abbr">Fase:</span> {{ isset($torneo_fase->TorneoFaseLeg->fase->nombre) ? $torneo_fase->TorneoFaseLeg->fase->nombre : '' }} </div>
+              <div class="glossary__item"><span class="glossary__abbr">Etapa:</span> {{ isset($torneo_fase->TorneoFaseLeg->nombre) ? $torneo_fase->TorneoFaseLeg->nombre : '' }} </div>
               <div class="glossary__item"><span class="glossary__abbr">Fecha:</span> {{$partido->fecha_inicio}} </div>
               <div class="glossary__item"><span class="glossary__abbr">Hora:</span> {{$partido->hora}} hs</div>
               <div class="glossary__item"><span class="glossary__abbr">Partido N°:</span> {{$partido->numero_partido}}</div>
